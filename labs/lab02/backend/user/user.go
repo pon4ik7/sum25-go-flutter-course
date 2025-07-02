cedec5973fade38222bbdf1adf110bc6db298b73
Rolan <muliukinrolan@mail.ru>
2025-07-02 10:40:29 +0300

    All tasks done

--- labs/lab02/backend/user/user.go
+++ labs/lab02/backend/user/user.go
@@ -9,11 +9,13 @@ import (
 )
 
 // User represents a chat user
-// TODO: Add more fields if needed
 var (
-	ErrInvalidName  = errors.New("invalid name: must be between 1 and 30 characters")
+	ErrInvalidName  = errors.New("invalid name: must be at least 1 character")
 	ErrInvalidAge   = errors.New("invalid age: must be between 0 and 150")
 	ErrInvalidEmail = errors.New("invalid email format")
+	ErrUserAlreadyExist = errors.New("user with this ID already exist")
+	ErrUserDoesNotExist = errors.New("user eith this ID does not exist")
+	ErrContextCansel = errors.New("cotext cancel")
 	emailRegexp = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
 )
 
@@ -21,6 +23,19 @@ type User struct {
 	Name  string
 	Email string
 	ID    string
+}
+
+func NewUser(name string, email string, id string) (User, error){
+	user := User{
+		Name: name,
+		Email: email,
+		ID: id,
+	}
+	err := user.Validate()
+	if err != nil{
+		return User{}, err
+	}
+	return user, nil
 }
 
 // Validate checks if the user data is valid
@@ -46,7 +61,7 @@ func IsValidEmail(email string) bool {
 
 // IsValidName checks if the name is valid, returns false if the name is empty or longer than 30 characters
 func IsValidName(name string) bool {
-	return len(name) <= 1 && len(name) >= 30
+	return len(name) >= 1
 }
 
 // IsValidAge checks if the age is valid, returns false if the age is not between 0 and 150
@@ -62,40 +77,62 @@ type UserManager struct {
 	ctx   context.Context
 	users map[string]User // userID -> User
 	mutex sync.RWMutex    // Protects users map
-	// TODO: Add more fields if needed
 }
 
 // NewUserManager creates a new UserManager
 func NewUserManager() *UserManager {
-	// TODO: Initialize UserManager fields
 	return &UserManager{
+		ctx: context.Background(),
 		users: make(map[string]User),
+		mutex: sync.RWMutex{},
 	}
 }
 
 // NewUserManagerWithContext creates a new UserManager with context
 func NewUserManagerWithContext(ctx context.Context) *UserManager {
-	// TODO: Initialize UserManager with context
 	return &UserManager{
 		ctx:   ctx,
 		users: make(map[string]User),
+		mutex: sync.RWMutex{},
 	}
 }
 
 // AddUser adds a user
 func (m *UserManager) AddUser(u User) error {
-	// TODO: Add user to map, check context
+	if err := m.ctx.Err(); err != nil {
+        return ErrContextCansel
+    }
+	m.mutex.Lock()
+    defer m.mutex.Unlock()
+
+    if _, exists := m.users[u.ID]; exists {
+        return ErrUserAlreadyExist
+    }
+
+    m.users[u.ID] = u
 	return nil
 }
 
 // RemoveUser removes a user
 func (m *UserManager) RemoveUser(id string) error {
-	// TODO: Remove user from map
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	if _, exist := m.users[id]; !exist {
+		return ErrUserDoesNotExist
+	} 
+
+	delete(m.users, id)
 	return nil
 }
 
 // GetUser retrieves a user by id
 func (m *UserManager) GetUser(id string) (User, error) {
-	// TODO: Get user from map
-	return User{}, errors.New("not found")
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+	user, exist := m.users[id]
+	if !exist {
+		return User{}, ErrUserDoesNotExist
+	}
+	return user, nil
 }
